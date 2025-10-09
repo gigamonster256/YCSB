70f49e65f6e5957c29a48df48e595d317cb1de74
Caleb Norton <n0603919@outlook.com>
2025-10-09 12:50:07 -0500

    benchmark framework

---
+++ benchmark.sh
@@ -0,0 +1,203 @@
+#!/usr/bin/env bash
+
+trap "exit" INT
+
+# global variables
+workload_dir="workloads"
+rocksdb_dir="/mnt/kvs/scratch/rocksdb"
+
+# usage function
+function usage {
+    echo "Usage: $0 <arg>"
+    echo "arg: help|h"
+    echo "arg: load [dbs] <record size> <record count>"
+    echo "arg: get [dbs] <record count>"
+    echo "arg: get_update [dbs] <record count>"
+    echo "arg: scan [dbs] <length> <record count>"
+    echo "arg: aging [dbs] <record count>"
+}
+
+function execute {
+    local type=$1
+    local db=$2
+    local workload_file=$3
+    local additional_args=$4
+
+    ./bin/ycsb $type $db -s -P $workload_file $additional_args 2>/dev/null \
+        | grep -E "RunTime|Throughput|AverageLatency|50thPercentileLatency|95thPercentileLatency|99thPercentileLatency|99.99PercentileLatency" \
+        | grep -Ev "CLEANUP" \
+        | awk '{print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16}'
+}
+
+function validate_record_count {
+    local record_count=$1
+
+    if [ ! -d "$workload_dir/$record_count" ]; then
+        echo Invalid record count $record_count
+        echo Valid record counts are: $(ls $workload_dir | grep -E "^[0-9]+M$" | tr '\n' ' '| sort)
+        exit 1
+    fi
+}
+
+function value_in_list {
+    local value=$1
+    local list=$2
+
+    for item in $list; do
+        if [ "$item" = "$value" ]; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+function load {
+    local dbs=$1
+    local record_size=${2:-64}
+    local record_count=${3:-1M}
+
+    validate_record_count $record_count
+
+    local file_prefix=$workload_dir/$record_count/load-
+
+    local valid_sizes=$(ls $file_prefix* | awk -F'-' '{print $NF}' | sort -n | uniq)
+
+    value_in_list $record_size "$valid_sizes"
+    local size_is_valid=$?
+
+    if [ $size_is_valid -ne 0 ]; then
+        echo Invalid record size $record_size
+        echo Valid record sizes are: $valid_sizes for record count $record_count
+        exit 0
+    fi
+
+    local workload_file="$file_prefix$record_size"
+
+    # /mnt/kvs/scratch/clean.sh
+    for db in $dbs; do
+        # echo Executing load on $db with record size $record_size and record count $record_count
+        execute load $db $workload_file "-p rocksdb.dir=$rocksdb_dir" # -p rocksdb.optionsfile=/mnt/kvs/ycsb/YCSB-0.15.0/rocksdbconfig.ini" # rocksdb args are harmless to other dbs
+        echo
+    done
+}
+
+function get {
+    local dbs=$1
+    local record_count=${2:-1M}
+
+    validate_record_count $record_count
+
+    local workload_file="$workload_dir/$record_count/get-50K"
+
+    for db in $dbs; do
+        # echo Executing get on $db with record count $record_count
+        execute run $db $workload_file "-p rocksdb.dir=$rocksdb_dir -p rocksdb.optionsfile=/mnt/kvs/ycsb/YCSB-0.15.0/rocksdbconfig.ini"
+        echo
+    done
+}
+
+function get_update {
+    local dbs=$1
+    local record_count=${2:-1M}
+
+    validate_record_count $record_count
+
+    local workload_file="$workload_dir/$record_count/get-up-50K"
+
+    for db in $dbs; do
+        # echo Executing get_update on $db with record count $record_count
+        execute run $db $workload_file "-p rocksdb.dir=$rocksdb_dir -p rocksdb.optionsfile=/mnt/kvs/ycsb/YCSB-0.15.0/rocksdbconfig.ini"
+        echo
+    done
+}
+
+function scan {
+    local dbs=$1
+    local length=${2:-100}
+    local record_count=${3:-1M}
+
+    validate_record_count $record_count
+
+    local file_prefix=$workload_dir/$record_count/scan-
+
+    local valid_lengths=$(ls $file_prefix* | awk -F'-' '{print $NF}' | sort -n | uniq)
+    
+    value_in_list $length "$valid_lengths"
+    local length_is_valid=$?
+
+    if [ $length_is_valid -ne 0 ]; then
+        echo Invalid scan length $length
+        echo Valid scan lengths are: $valid_lengths for record count $record_count
+        exit 0
+    fi
+
+    local workload_file="$file_prefix$length"
+
+    for db in $dbs; do
+        # echo Executing scan on $db with length $length and record count $record_count
+        execute run $db $workload_file "-p rocksdb.dir=$rocksdb_dir"
+        echo
+    done
+}
+
+function aging {
+    local dbs=$1
+    local record_count=${2:-1M}
+
+    validate_record_count $record_count
+
+    local workload_file="$workload_dir/$record_count/aging"
+
+    for db in $dbs; do
+        # echo Executing aging on $db with record count $record_count
+        execute run $db $workload_file "-p rocksdb.dir=$rocksdb_dir"
+        echo
+    done
+}
+
+# usage
+if [ $# -lt 2 ]; then
+    usage
+    exit 1
+fi
+
+if [ $1 = "help" ] || [ $1 = "h" ]; then
+    usage
+    exit 0
+fi
+
+# load
+if [ $1 = "load" ]; then
+    load "$2" $3 $4
+    exit 0
+fi
+
+# get
+if [ $1 = "get" ]; then
+    get "$2" $3
+    exit 0
+fi
+
+# get_update
+if [ $1 = "get_update" ]; then
+    get_update "$2" $3
+    exit 0
+fi
+
+# scan
+if [ $1 = "scan" ]; then
+    scan "$2" $3 $4
+    exit 0
+fi
+
+# aging
+if [ $1 = "aging" ]; then
+    aging "$2" $3
+    exit 0
+fi
+
+
+
+
+
---
+++ workload_runner.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+
+trap "exit" INT
+
+# record_counts="1M 10M 50M 100M"
+record_counts="50M"
+# load_sizes="64 256 1k 4k"
+load_sizes="64"
+# 512 1k"
+# 512 1k 4k"
+# scan_lengths="100"
+# scan_lengths="50 100 500"
+scan_lengths=""
+aging_rounds=0
+do_compaction=0
+do_gets=0
+do_get_update=0
+aging_last=0
+
+results_dir="results/intial_50M"
+# dbs="snap3db"
+# dbs="mixdb rocksdb wisckey snap3db"
+dbs="rocksdb wisckey"
+# snapdb wisckey rocksdb snap3db mixdb"
+
+if [ ! -d "$results_dir" ]; then
+	mkdir -p $results_dir
+fi
+
+for record_count in $record_counts; do
+	for load_size in $load_sizes; do
+		echo "Cleaning old databases"
+		/mnt/kvs/scratch/clean.sh
+		run_name="loads with record size $load_size and record count $record_count"
+		echo "Running $run_name"
+		for db in $dbs; do
+			beginning_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+			output_file="$results_dir/load-$load_size-$record_count-$db"
+			/usr/bin/time -v ./benchmark.sh load $db $load_size $record_count >$output_file.csv 2>$output_file.time
+			after_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+			echo "Writes: $((after_writes - beginning_writes))" >>$output_file.time
+		done
+
+		# save size of dbs
+		echo "Saving size of dbs"
+		for db in $dbs; do
+			du /mnt/kvs/scratch/$db >>$results_dir/load_size-$load_size-$record_count.txt
+		done
+
+		# save kdb size
+		echo "Saving kdb size"
+		du /mnt/kvs/scratch/snapdb/keydb.tkmb >>$results_dir/load_size-$load_size-$record_count.txt
+		du /mnt/kvs/scratch/snap3db/keydb.tkmb >>$results_dir/load_size-$load_size-$record_count.txt
+
+		# run aging last if set to 1
+		if [ $aging_last -eq 0 ]; then
+			for aging_round in $(seq 1 $aging_rounds); do
+				run_name="aging with record count $record_count" # load_size is implicit based on what was last loaded
+				echo "Running $run_name"
+				for db in $dbs; do
+					output_file="$results_dir/aging-$load_size-$record_count-$aging_round-$db"
+					beginning_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+					/usr/bin/time -v ./benchmark.sh aging $db $record_count >$output_file.csv 2>$output_file.time
+					after_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+					echo "Writes: $((after_writes - beginning_writes))" >>$output_file.time
+				done
+
+				# # dump garbage collection stats
+				# echo "Saving garbage collection stats"
+				# /mnt/kvs/wisckey_caleb/test/test_snap d /mnt/kvs/scratch/snapdb &>$results_dir/gc-$load_size-$record_count-$aging_round.txt
+
+				# save size of db
+				echo "Saving size of dbs"
+				for db in $dbs; do
+					du /mnt/kvs/scratch/$db >>$results_dir/aging_size-$load_size-$record_count-$aging_round.txt
+				done
+			done
+		fi
+
+		# run compaction on snapdb if set to 1
+		if [ $do_compaction -eq 1 ]; then
+			echo "Running compaction"
+			/usr/bin/time -v /mnt/kvs/wisckey_caleb/test/test_snap c /mnt/kvs/scratch/snapdb &>$results_dir/compact-$load_size-$record_count.time
+
+			# print size of dbs
+			echo "Saving size of dbs"
+			for db in $dbs; do
+				du /mnt/kvs/scratch/$db >>$results_dir/compact_size-$load_size-$record_count.txt
+			done
+		fi
+
+		# run gets
+		if [ $do_gets -eq 1 ]; then
+			run_name="gets with record count $record_count" # load_size is implicit based on what was last loaded
+			echo "Running $run_name"
+			for db in $dbs; do
+				output_file="$results_dir/get-$load_size-$record_count-$db"
+				beginning_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+				/usr/bin/time -v ./benchmark.sh get $db $record_count >$output_file.csv 2>$output_file.time
+				after_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+				echo "Writes: $((after_writes - beginning_writes))" >>$output_file.time
+			done
+		fi
+
+		if [ $do_get_update -eq 1 ]; then
+			run_name="get_updates with record count $record_count" # load_size is implicit based on what was last loaded
+			echo "Running $run_name"
+			for db in $dbs; do
+				output_file="$results_dir/get_update-$load_size-$record_count-$db"
+				beginning_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+				/usr/bin/time -v ./benchmark.sh get_update $db $record_count >$output_file.csv 2>$output_file.time
+				after_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+				echo "Writes: $((after_writes - beginning_writes))" >>$output_file.time
+			done
+		fi
+
+		# run scans
+		for scan_length in $scan_lengths; do
+			run_name="scans with length $scan_length and record count $record_count" # load_size is implicit based on what was last loaded
+			echo "Running $run_name"
+			for db in $dbs; do
+				output_file="$results_dir/scan-$load_size-$scan_length-$record_count-$db"
+				beginning_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+				/usr/bin/time -v ./benchmark.sh scan $db $scan_length $record_count >$output_file.csv 2>$output_file.time
+				after_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+				echo "Writes: $((after_writes - beginning_writes))" >>$output_file.time
+			done
+		done
+
+		# run aging last if set to 1
+		if [ $aging_last -eq 1 ]; then
+			for aging_round in $(seq 1 $aging_rounds); do
+				run_name="aging with record count $record_count"
+				echo "Running $run_name"
+				for db in $dbs; do
+					output_file="$results_dir/aging-$load_size-$record_count-$aging_round-$db"
+					beginning_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+					/usr/bin/time -v ./benchmark.sh aging $db $record_count >$output_file.csv 2>$output_file.time
+					after_writes=$(cat /sys/block/nvme1n1/stat | awk '{print $7}')
+					echo "Writes: $((after_writes - beginning_writes))" >>$output_file.time
+				done
+
+				# # dump garbage collection stats
+				# echo "Saving garbage collection stats"
+				# /mnt/kvs/wisckey_caleb/test/test_snap d /mnt/kvs/scratch/snapdb &>$results_dir/gc-$load_size-$record_count-$aging_round.txt
+
+				# save size of db
+				echo "Saving size of dbs"
+				for db in $dbs; do
+					du /mnt/kvs/scratch/$db >>$results_dir/aging_size-$load_size-$record_count-$aging_round.txt
+				done
+			done
+		fi
+	done
+done
